ca06dc949dc1e743e734e04ed134f02e712d82f2
duvan-23 <duvamendi2@hotmail.com>
2025-01-14 01:09:52 -0500

    slot machine router

---
+++ routes/slot-machine.js
@@ -0,0 +1,9 @@
+import Router from "express";
+import * as slotController from '../controllers/slot-machine.js';
+import { validateCalculate } from "../middleware/validationRequest.js"
+const router = Router();
+
+router.get("/", slotController.getParametersGame);
+router.put("/",validateCalculate, slotController.calculateResult);
+
+export default router;
