bc45720a75891edc4bd5af76d89c4439ecc576da
duvan-23 <duvamendi2@hotmail.com>
2025-01-14 01:07:35 -0500

    adjustment json response

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -4,7 +4,7 @@ export const getAllGames = async (req, res) => {
     try {
         //get games
         const games = await gamesModel.getAllGames();
-        res.status(200).json({ games });
+        res.status(200).json( games );
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch games', message: error.message });
     }
@@ -15,7 +15,7 @@ export const getGamesByName = async (req, res) => {
     try {
         // get games after filter
         const games = await gamesModel.getGamesByName(title);
-        res.status(200).json({ games });
+        res.status(200).json( games );
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch games', message: error.message });
     }
