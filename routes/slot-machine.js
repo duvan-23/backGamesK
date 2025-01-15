6700c8f44d01d07810f6d04a91d5a42b9ed9ac5b
duvan-23 <duvamendi2@hotmail.com>
2025-01-15 01:28:25 -0500

    add semicolon

--- routes/slot-machine.js
+++ routes/slot-machine.js
@@ -1,6 +1,6 @@
 import Router from "express";
 import * as slotController from '../controllers/slot-machine.js';
-import { validateCalculate } from "../middleware/validationRequest.js"
+import { validateCalculate } from "../middleware/validationRequest.js";
 const router = Router();
 
 router.get("/", slotController.getParametersGame);
