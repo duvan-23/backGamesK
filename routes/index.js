35dc336b85cdca404855e1527e3960cf5bf4001f
duvan-23 <duvamendi2@hotmail.com>
2025-01-15 22:56:32 -0500

    router adjustment

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,12 @@
-import fs from 'fs';
 import Router from "express";
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
+import currencyRoutes from "./currency.js";
+import gamesRoutes from "./games.js";
+import slotRoutes from "./slot-machine.js";
 
 const router = Router();
 
-const removeExtension = (fileName)=>{
-    return fileName.split('.').shift();
-}
-
-const fileNames  = fs.readdirSync(__dirname);
-fileNames.forEach((fileName)=>{
-    // get the name of the file
-    const name = removeExtension(fileName);
-    if(name!="index" && name!="auth"){
-        router.use(`/${name}`, async(req, res, next)=>{
-            try{
-                //Import the router from the selected file
-                const module = await import(`./${fileName}`);
-                module.default(req,res, next)
-            }catch(e){
-                next(e);
-            }
-        });
-    }
-});
-
+router.use('/currency',currencyRoutes);
+router.use('/games',gamesRoutes);
+router.use('/slot-machine',slotRoutes);
 
 export default router;
