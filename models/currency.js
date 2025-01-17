c1f10e6d35f57bd12f8ce6ac99a46c89141beeb1
duvan-23 <duvamendi2@hotmail.com>
2025-01-17 01:23:54 -0500

    error typo

--- models/currency.js
+++ models/currency.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import 'dotenv/config';
 
 export const currency = async() => {
-    //Consult external currency exchange api 
+    //Consult external exchange currency api 
     return  await axios.get(process.env.URLCURRENCY);
 };
